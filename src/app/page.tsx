d936f711fdea9ea8e977a0910e3c0bab06e94bec
Tan ZL <tanzl88@gmail.com>
2023-09-11 22:49:30 +0800

    Make UI dynamic based on stakedAmount

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,72 +1,92 @@
 "use client";
 
-import React, { useState, useEffect } from "react";
-import { Web3Button } from "@web3modal/react";
+import React, {useState, useEffect} from "react";
+import {Web3Button} from "@web3modal/react";
 import StakeInput from "./stake";
 import WithdrawInput from "./withdraw";
-import { useAccount, useBalance, useNetwork, useContractRead } from "wagmi";
+import {useAccount, useBalance, useNetwork, useContractRead} from "wagmi";
 import StakingContractABI from "../ABI/StakingContract.json";
-import { ADDRESSES } from "@/app/addresses";
+import {ADDRESSES} from "@/app/addresses";
+import {utils} from "web3";
 
 export default function StakingComponent() {
-    const [refresh, setRefresh] = useState(false);
-    const { chain, chains } = useNetwork();
-    const { address, isConnected } = useAccount();
-    const balance = useBalance({
-        address: address,
-        token: ADDRESSES.MockToken,
-    });
+  const [refresh, setRefresh] = useState(false);
+  const {chain, chains} = useNetwork();
+  const {address, isConnected} = useAccount();
+  const balance = useBalance({
+    address: address,
+    token: ADDRESSES.MockToken,
+  });
 
-    const isWhitelisted = useContractRead({
-        address: ADDRESSES.StakingContract,
-        abi: StakingContractABI,
-        functionName: "isWhitelisted",
-        args: [address],
-    });
+  const isWhitelisted = useContractRead({
+    address: ADDRESSES.StakingContract,
+    abi: StakingContractABI,
+    functionName: "isWhitelisted",
+    args: [address],
+  });
 
-    const stakedAmount = useContractRead({
-        address: ADDRESSES.StakingContract,
-        abi: StakingContractABI,
-        functionName: "getStakedAmount",
-        args: [address],
-    });
+  const stakedAmount = useContractRead({
+    address: ADDRESSES.StakingContract,
+    abi: StakingContractABI,
+    functionName: "getStakedAmount",
+    args: [address],
+    watch: true
+  });
 
-    useEffect(() => {}, []);
-
-    if (!isConnected) {
-        return (
-            <div>
-                <Web3Button />
-            </div>
-        );
+  useEffect(() => {
+    if (refresh) {
+      refetchStakedAmount();
     }
+  }, [refresh]);
 
-    if (isWhitelisted?.data !== true) {
-        return (
-            <div>
-                <p>You are not whitelisted</p>
-            </div>
-        );
-    }
+  async function refetchStakedAmount() {
+    const newData = await stakedAmount?.refetch();
+    console.log("NEW DATA", newData);
+    setRefresh(false);
+  }
 
+
+  const toWei = (amount: string) => {
+    if (!amount) return null;
+
+    return utils.fromWei(amount, "ether");
+  };
+
+  if (!isConnected) {
     return (
-        <div>
-            <h1>Staking and Withdrawal</h1>
-            <p>Chain: {chain?.id}</p>
-            <p>Your address: {address}</p>
-            <p>
-                Balance: {balance?.data?.formatted} {balance?.data?.symbol}
-            </p>
-
-            <br />
-            <br />
-
-            <StakeInput setRefresh={setRefresh} />
-
-            <br />
-            <br />
-
-            <WithdrawInput setRefresh={setRefresh} stakedAmount={stakedAmount?.data as BigInt} />
-        </div>
+      <div>
+        <Web3Button/>
+      </div>
     );
+  }
+
+  if (isWhitelisted?.data !== true) {
+    return (
+      <div>
+        <p>You are not whitelisted</p>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Staking and Withdrawal</h1>
+      <p>Chain: {chain?.id}</p>
+      <p>Your address: {address}</p>
+      <p>
+        Balance: {balance?.data?.formatted} {balance?.data?.symbol}
+      </p>
+      <p>Amount staked: {toWei(stakedAmount?.data?.toString(10))}</p>
+
+      <br/>
+      <br/>
+
+      {
+        stakedAmount?.data.toString() !== '0' ?
+          <WithdrawInput setRefresh={setRefresh} /> :
+          <StakeInput setRefresh={setRefresh} />
+      }
+</div>
+)
+  ;
 }
--- src/app/withdraw.tsx
+++ src/app/withdraw.tsx
@@ -6,7 +6,7 @@ import { usePrepareContractWrite, useContractWrite } from "wagmi";
 import StakingContractABI from "../ABI/StakingContract.json";
 import { ADDRESSES } from "@/app/addresses";
 
-export default function WithdrawInput({ setRefresh, stakedAmount }: { setRefresh: Function, stakedAmount: BigInt }) {
+export default function WithdrawInput({ setRefresh }: { setRefresh: Function }) {
     const { config: withdrawContractWriteConfig } = usePrepareContractWrite({
         address: ADDRESSES.StakingContract,
         abi: StakingContractABI,
@@ -30,16 +30,9 @@ export default function WithdrawInput({ setRefresh, stakedAmount }: { setRefresh
         withdraw?.();
     };
 
-    const toWei = (amount: string) => {
-        if (!amount) return null;
-
-        return utils.fromWei(amount, "ether");
-    };
-
     return (
         <div>
             <h2>Withdraw</h2>
-            <p>Amount staked: {toWei(stakedAmount?.toString(10))}</p>
             <button onClick={handleWithdraw} style={{ padding: "10px 20px", marginTop: "10px", background: "#ef5e91" }}>
                 Withdraw
             </button>
